fe5adf32c0e555b5306484c9f3918bcb63e764ac
Dm-prog <prostokota@list.ru>
2020-10-04 22:40:10 +0400

    Game 20 matches

--- epam - projects/TwentyMatches/src/TwentyMatches.java
+++ epam - projects/TwentyMatches/src/TwentyMatches.java
@@ -2,43 +2,58 @@
 import java.util.Scanner;
 
 public class TwentyMatches {
+    private static int sum = 17;
+    private static int countMatches = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("The game started...");
-        int sum = 17;
-        System.out.println("Computer took 3 matches, 17 matches left");
+        System.out.println("На столе осталось 20 спичек");
+        System.out.println("- Количество выбранных компьютером спичек = 3. \n" +
+                "На столе осталось 17 спичек");
+        System.out.print("- Ход игрока. Введите количество спичек: ");
 
         while (sum > 1) {
-            if (!scanner.hasNextInt()) {
-                System.out.println("You only need to enter integers.");
-                break;
-            }
             int player = scanner.nextInt();
-            int countMatches = 0;
             if (player == 1) {
                 sum -= player;
-                System.out.println("Player took " + player + " match, " + sum + " matches left");
+                System.out.println("На столе осталось " + sum + " спичек");
                 countMatches = 3;
                 sum -= countMatches;
-                System.out.println("Computer took " + countMatches + " matches, " + sum + " matches left");
+                System.out.println("- Количество выбранных компьютером спичек = " + countMatches + ".");
+                if (sum == 1) {
+                    System.out.println("Для игрока осталась последняя спичка. Игрок проиграл!");
+                    return;
+                }
+                System.out.println("На столе осталось " + sum + " спичек");
+                System.out.print("- Ход игрока. Введите количество спичек: ");
             } else if (player == 2) {
                 sum -= player;
-                System.out.println("Player took " + player + " matches, " + sum + " matches left");
+                System.out.println("На столе осталось " + sum + " спичек");
                 countMatches = 2;
                 sum -= countMatches;
-                System.out.println("Computer took " + countMatches + " matches, " + sum + " matches left");
+                System.out.println("- Количество выбранных компьютером спичек = " + countMatches + ".");
+                if (sum == 1) {
+                    System.out.println("Для игрока осталась последняя спичка. Игрок проиграл!");
+                    return;
+                }
+                System.out.println("На столе осталось " + sum + " спичек");
+                System.out.print("- Ход игрока. Введите количество спичек: ");
             } else if (player == 3) {
                 sum -= player;
-                System.out.println("Player took " + player + " matches, " + sum + " matches left");
+                System.out.println("На столе осталось " + sum + " спичек");
                 countMatches = 1;
                 sum -= countMatches;
-                System.out.println("Computer took " + countMatches + " match, " + sum + " matches left");
+                System.out.println("- Количество выбранных компьютером спичек = " + countMatches + ".");
+                if (sum == 1) {
+                    System.out.println("Для игрока осталась последняя спичка. Игрок проиграл!");
+                    return;
+                }
+                System.out.println("На столе осталось " + sum + " спичек");
+                System.out.print("- Ход игрока. Введите количество спичек: ");
             } else {
-                System.out.println("Player took the wrong number of matches, try again.");
-            }
-            if (sum == 1) {
-                System.out.println("The game is over. Computer won!");
-                return;
+                System.out.println("Некорректное количество спичек!");
+                System.out.println("На столе осталось " + sum + " спичек");
+                System.out.print("- Ход игрока. Введите количество спичек: ");
             }
         }
     }
